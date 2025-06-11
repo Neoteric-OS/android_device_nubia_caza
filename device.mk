99f8d239f0ee12ae9e4af997a3cde3801105ff93
Cosmin Tanislav <demonsingur@gmail.com>
2025-06-11 12:08:11 +0300

    caza: import hals.conf
    Change-Id: I1b1e7c4da548d8586ba4069c3a3aeb5344ca5f92

--- device.mk
+++ device.mk
@@ -276,6 +276,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.multihal
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH) \
