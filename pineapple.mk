76ebee150558e42f334fdc8ffe435247e07010ca
Akash Srivastava <akashniki@gmail.com>
2025-11-10 19:03:46 +0100

    caza: Enable back stream effect for vibrator hal
    Change-Id: Ib24eb34fee4dcad917f49248c336434c397904c0

--- pineapple.mk
+++ pineapple.mk
@@ -536,6 +536,7 @@ PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 $(call soong_config_set,qti_vibrator,effect_lib,libqtivibratoreffect.nubia_caza-richtap)
+$(call soong_config_set,qti_vibrator,use_effect_stream,true)
 
 # Weaver
 PRODUCT_PACKAGES += \
